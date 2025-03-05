2494c8839b54079f9c93f6a6e1a6396ec161c54e
user <>
2025-03-05 21:18:12 +0900

    gitignore test

--- src/main/java/com/example/test/config/FirebaseConfig.java
+++ src/main/java/com/example/test/config/FirebaseConfig.java
@@ -4,7 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Configuration;
 
 import java.io.InputStream;
 
